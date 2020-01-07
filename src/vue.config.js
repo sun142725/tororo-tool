ac974e3e9648728faaa4ce4fff7c6c9d0e31c039
sun142725 <18334771358s>
2020-01-07 15:14:23 +0800

    初始化目录

---
+++ src/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    lintOnSave: false,
+    // css的处理
+    css: {
+      loaderOptions: {
+        sass: {
+        //   prependData: `@import "~@/assets/style/main.scss";`
+        }
+      }
+    }
+}
