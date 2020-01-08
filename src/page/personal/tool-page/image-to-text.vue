4385f35fb85c56c130555dadf13c2f87ebf5b259
sun142725 <18334771358s>
2020-01-08 18:04:17 +0800

    新增文件

---
+++ src/page/personal/tool-page/image-to-text.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <input type="file" ref="">
+</div>
+</template>
+<script>
+export default{
+    name: 'image-to-text',
+    data: function(){
+        return {
+            
+        }
+    },
+    methods: {
+        // if (imgBase.split(',')[0].indexOf('base64') >= 0) {
+                    // imgBase64 = imgBase.split(',')[1];
+                    // getText(imgBase64)
+                // }
+        getImgBase64(image){
+            var reader = new FileReader();
+            reader.readAsDataURL(image);
+            reader.onload = function (e) {
+                console.log(e)
+                return e.target.result; //这个就是base64的编码
+            }
+        }
+    }
+}
+</script>
